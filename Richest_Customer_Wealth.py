ff6522874f3e06540a2488409e745a1b687d2ffb
William Deng <84593442+Quantumplations@users.noreply.github.com>
2025-06-19 10:56:51 +0100

    Create Richest_Customer_Wealth.py

---
+++ Richest_Customer_Wealth.py
@@ -0,0 +1,13 @@
+# My original solution:
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        m, n = len(accounts), len(accounts[0])
+        wealths = [None] * m
+        for i in range(m):
+            wealths[i] = sum(accounts[i])
+        return max(wealths)
+
+# Others' solutions:
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return max(map(sum, accounts))
